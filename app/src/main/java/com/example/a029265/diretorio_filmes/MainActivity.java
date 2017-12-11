8767b97bea8389d9b6a42a951d179d433af82a66
Rui Barbosa <ruifnbarbosa@gmail.com>
2017-12-11 15:17:08 +0000

    Adicionado Layout Base

--- app/src/main/java/com/example/a029265/diretorio_filmes/MainActivity.java
+++ app/src/main/java/com/example/a029265/diretorio_filmes/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a029265.diretorio_filmes;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -10,4 +11,5 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
 }
